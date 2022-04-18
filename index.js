57336ecd2d805ccf459b9a2dc4839680159c0f91
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-04-18 19:35:22 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const btns = document.querySelectorAll('button');
+let player = 'circle';
+btns.forEach((btn) =>
+  btn.addEventListener('click', (event) => {
+    /*if (btn.disabled) {
+      return;
+    } else {*/
+    if (player == 'circle') {
+      console.log(player);
+      event.target.className = 'board__field--circle';
+      btn.innerHTML = '<img id="circle" src="images/circle.svg" alt="circle"/>';
+      btn.disabled = true; // later I can delete
+      player = 'cross';
+      document.querySelector('#hraje').innerHTML =
+        'HRAJE: <img id="cross" src="images/cross.svg" alt="cross"/>';
+    } else {
+      console.log(player);
+      event.target.className = 'board__field--cross';
+      btn.innerHTML = '<img id="cross" src="images/cross.svg" alt="cross"/>'; //delete and replace in style.css
+      btn.disabled = true; // later i can delete
+      player = 'circle';
+      document.querySelector('#hraje').innerHTML =
+        'HRAJE: <img id="circle" src="images/circle.svg" alt="circle"/>';
+      // }
+      //btn.disabled = true;
+    }
+  }),
+);
